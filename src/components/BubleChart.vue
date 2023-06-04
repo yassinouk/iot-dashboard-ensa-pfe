3a50d326f68d75f47f2895b10e769d8faec9b16a
krank56 <58428792+krank56@users.noreply.github.com>
2023-06-04 12:46:47 +0100

    zabbbbbbb

--- src/components/BubleChart.vue
+++ src/components/BubleChart.vue
@@ -1,54 +1,72 @@
 <script setup lang="ts">
-import Chart from 'chart.js/auto';
-import { onMounted, ref } from 'vue';
-
+import Chart from "chart.js/auto";
+import { onMounted, ref, watch, toRefs } from "vue";
+import { useMqttData } from "../composables/useMqttData";
 
 const abi = ref<HTMLCanvasElement>();
 
-const labels = [
-    '10:00',
-    '10:00',
-    '10:01',
-    '10:01',
-    '10:02',
-    '10:02',
-    '10:03',
-    '10:03',
-    '10:04',
-    '10:04',
-    '10:05',
-];
+const chart = ref();
 
-const data = {
-    labels,
-    datasets: [{
-        label: '°C',
-        backgroundColor: 'rgb(121, 224, 132)',
-        borderColor: 'rgb(71, 169, 146)',
-        data: [25.19, 25.3, 25.3, 25.2, 25.3, 25.25, 25.32, 25.2, 25.21, 25.19, 25.19],
-        tension: 0.3
-    }]
+const addData = (chart: Chart, label: string, data: number) => {
+  chart.data?.labels?.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+  chart.update();
 };
 
+const { data } = toRefs(useMqttData);
 
-
-
-onMounted(() => {
-    const ctx = abi.value?.getContext('2d');
-    if (ctx) {
-        new Chart(ctx, {
-            type: 'line',
-            data,
-        });
-    }
+watch(data, (receivedData) => {
+  console.log(receivedData);
+  if (chart.value) {
+    addData(chart.value, "°C", receivedData);
+  }
 });
 
+const labels = [
+  "10:00",
+  "10:00",
+  "10:01",
+  "10:01",
+  "10:02",
+  "10:02",
+  "10:03",
+  "10:03",
+  "10:04",
+  "10:04",
+  "10:05",
+];
 
+const chartData = {
+  labels,
+  datasets: [
+    {
+      label: "°C",
+      backgroundColor: "rgb(121, 224, 132)",
+      borderColor: "rgb(71, 169, 146)",
+      data: [
+        25.19, 25.3, 25.3, 25.2, 25.3, 25.25, 25.32, 25.2, 25.21, 25.19, 25.19,
+      ],
+      tension: 0.3,
+    },
+  ],
+};
+
+onMounted(() => {
+  const ctx = abi.value?.getContext("2d");
+  if (ctx) {
+    chart.value = new Chart(ctx, {
+      type: "line",
+      data: chartData,
+    });
+  }
+});
 </script>
 
 <template>
-    <h2>Temperature °C</h2>
-    <div>
-        <canvas ref="abi"></canvas>
-    </div>
+  <h2>Temperature °C</h2>
+  <div>
+    <canvas ref="abi"></canvas>
+  </div>
 </template>
