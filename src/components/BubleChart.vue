ae45e9393986fe0d4c55af21b95a8fed430e162f
krank56 <58428792+krank56@users.noreply.github.com>
2023-06-04 13:19:20 +0100

    zabbbbbbb

--- src/components/BubleChart.vue
+++ src/components/BubleChart.vue
@@ -27,41 +27,38 @@ socket.onmessage = (event: MessageEvent) => {
 
 const chart = ref();
 
-const labels = [
-  "10:00",
-  "10:00",
-  "10:01",
-  "10:01",
-  "10:02",
-  "10:02",
-  "10:03",
-  "10:03",
-  "10:04",
-  "10:04",
-  "10:05",
-];
-
-const chartData = {
-  labels,
-  datasets: [
-    {
-      label: "°C",
-      backgroundColor: "rgb(121, 224, 132)",
-      borderColor: "rgb(71, 169, 146)",
-      data: [
-        25.19, 25.3, 25.3, 25.2, 25.3, 25.25, 25.32, 25.2, 25.21, 25.19, 25.19,
-      ],
-      tension: 0.3,
-    },
-  ],
-};
-
 onMounted(() => {
   const ctx = abi.value?.getContext("2d");
   if (ctx) {
     chart.value = new Chart(ctx, {
       type: "line",
-      data: chartData,
+      data: {
+        labels: [
+          "10:00",
+          "10:00",
+          "10:01",
+          "10:01",
+          "10:02",
+          "10:02",
+          "10:03",
+          "10:03",
+          "10:04",
+          "10:04",
+          "10:05",
+        ],
+        datasets: [
+          {
+            label: "°C",
+            backgroundColor: "rgb(121, 224, 132)",
+            borderColor: "rgb(71, 169, 146)",
+            data: [
+              25.19, 25.3, 25.3, 25.2, 25.3, 25.25, 25.32, 25.2, 25.21, 25.19,
+              25.19,
+            ],
+            tension: 0.3,
+          },
+        ],
+      },
     });
   }
 });
