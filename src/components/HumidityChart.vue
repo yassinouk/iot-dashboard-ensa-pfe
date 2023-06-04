9e1ea8893e6a66b0f0009518a000d65a7bd8499e
krank56 <58428792+krank56@users.noreply.github.com>
2023-06-04 13:46:22 +0100

    zabbbbbbb

--- src/components/HumidityChart.vue
+++ src/components/HumidityChart.vue
@@ -1,53 +1,50 @@
 <script setup lang="ts">
-import Chart from 'chart.js/auto';
-import { onMounted, ref } from 'vue';
+import Chart from "chart.js/auto";
+import { onMounted, ref, watch, toRefs } from "vue";
 
+import { useMqttData, updateChart } from "../composables/useMqttData";
+
+const { data } = toRefs(useMqttData);
 
 const hum = ref<HTMLCanvasElement>();
 
-const labels = [
-    'humidity level',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-];
+let chart: Chart;
 
-const data = {
-    labels,
-    datasets: [{
-        label: '%',
-        backgroundColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgb(183, 183, 183)',
-        data: [60, 61, 60, 61, 59, 60, 59, 60, 59, 61, 60],
-    }]
-};
-
-
-
-
-onMounted(() => {
-    const ctx = hum.value?.getContext('2d');
-    if (ctx) {
-        new Chart(ctx, {
-            type: 'bar',
-            data,
-        });
-    }
+watch(data, (receivedData) => {
+  console.log(receivedData);
+  if (chart) {
+    updateChart(chart, receivedData);
+  }
 });
 
+const labels = ["humidity level", "", "", "", "", "", "", "", "", "", ""];
 
+const chartData = {
+  labels,
+  datasets: [
+    {
+      label: "%",
+      backgroundColor: "rgb(75, 192, 192)",
+      borderColor: "rgb(183, 183, 183)",
+      data: [60, 61, 60, 61, 59, 60, 59, 60, 59, 61, 60],
+    },
+  ],
+};
+
+onMounted(() => {
+  const ctx = hum.value?.getContext("2d");
+  if (ctx) {
+    chart = new Chart(ctx, {
+      type: "bar",
+      data: chartData,
+    });
+  }
+});
 </script>
 
 <template>
-    <h2>Humidity</h2>
-    <div>
-        <canvas ref="hum"></canvas>
-    </div>
+  <h2>Humidity</h2>
+  <div>
+    <canvas ref="hum"></canvas>
+  </div>
 </template>
